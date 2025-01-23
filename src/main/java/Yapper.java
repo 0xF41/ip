06fbbab98bf6a6931226220952f3e0a574d88cf2
0xF41 <faiyew@DESKTOP-ARR737S.>
2025-01-23 11:18:11 +0800

    Add level 3

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Yapper {
 
-    public static final String NAME = "Yapper"; 
-
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));; 
 
-    public static enum COMMANDS {LIST, BLAH, BYE}
+    public static final String NAME = "Yapper"; 
 
+    public static ArrayList<String> items = new ArrayList<>();
 
     private static void greet() {
         System.out.println("____________________________________________________________");
@@ -20,19 +20,30 @@ public class Yapper {
     }   
 
     private static void startConversation() throws IOException {
+
         String cmd = "";
         do {
-            String[] tokens = br.readLine().split(" ");
-            cmd = tokens[0];
-            if (!cmd.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(cmd);
-                System.out.println("____________________________________________________________");
-            } else {
+            cmd = br.readLine();
+
+            if (cmd.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
+                return;
+            }  
+            if (cmd.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0, n = items.size(); i < n; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, items.get(i)));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                items.add(cmd);
+                System.out.println("____________________________________________________________");
+                System.out.println(String.format("Added: %s", cmd));
+                System.out.println("____________________________________________________________");
             }
+
         } while (!cmd.equals("bye"));
     }
 
