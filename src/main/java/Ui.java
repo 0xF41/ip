e42aee846e9e71e4f9dc5a38997828f4be8da94b
0xF41 <faiyew@DESKTOP-ARR737S.>
2025-01-28 21:25:02 +0800

    Update README.md

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -10,7 +10,7 @@ public class Ui {
         System.out.println("____________________________________________________________");
     }
 
-    public static void printHelp() {
+    public static void printMenu() {
         String menu = """
             __   __
             \\ \\ / /_ _ _ __  _ __   ___ _ __
@@ -25,9 +25,9 @@ public class Ui {
             unmark <task_number>  - Unmark task with <task_number> as incomplete
             todo <task_name>      - Create a new task specified with <task_name>
             deadline <task_name> /by <deadline>
-                                  - Create a new task with a deadline, specifying <deadline> as the task deadline
+                                  - Create a new Deadline task with a <deadline> with <dd-MM-yyyy HHmm> format
             event <task_name> /from <start_time> /to <end_time>
-                                  - Create a new Event task with a <start_time> and <end_time>
+                                  - Create a new Event task with a <start_time> and <end_time> with <dd-MM-yyyy HHmm> format
             delete <task_number>  - Delete task with <task_number> on the list
             bye                   - End the conversation with the chatbot
             help                  - Show this help menu
